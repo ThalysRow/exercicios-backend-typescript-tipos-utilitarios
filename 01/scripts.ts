7374413f79e43da29e8da01e502b43aabd8cd1ea
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-11-17 22:57:44 -0300

    Finalizado exercício 01

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,17 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+  nome: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  dataNacimento?: string;
+};
 
-const cadastrarUsuário = (info: Usuario): Usuario => {
-    return info;
-}
+type NovoUsuario = Omit<Usuario, "rg">;
+
+const cadastrarUsuario = (info: Usuario): Usuario => {
+  return info;
+};
+
+const novoCadastro = (info: Omit<Usuario, "rg">): NovoUsuario => {
+  return info;
+};
