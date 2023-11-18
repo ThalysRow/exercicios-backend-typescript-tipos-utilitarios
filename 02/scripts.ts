92666fe78b6cf1810b709342ed144c50b407f04c
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-11-18 19:41:55 -0300

    finalizado exercício 02

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,52 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+  nome: string;
+  descricao: string;
+  valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+  numero: number;
+  validade: string;
+  nome: string;
+  cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: "credito" | "Debito";
+  cartao: Cartao;
+};
+
+type Transacao = "credito" | "debito";
+
+type Pagamento = Lowercase<Transacao>;
+
+type Endereco = {
+  cep: string;
+  rua: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+};
+
+// Pode ser assim:
+
+type NovoCarrinho = {
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  endereco: Endereco;
+  transacao: Pagamento;
+  cartao: Cartao;
+};
+
+// Ou assim:
+
+type NovoCarrinho2 = Omit<Carrinho, "tipoTransacao"> & {
+  endereco: Endereco;
+  transacao: Pagamento;
+};
